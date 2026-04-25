578f6699e9fe480884e3867d5c515db19477e4b4
Lucas <lucascapel@college-louezdieu.fr>
2026-04-25 10:53:20 +0200

    Add files via upload

---
+++ android/app/src/main/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase config — configuré ✅
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyAft-QF2fShtICt865nFQgRwCROTRYuZTg",
+  authDomain:        "dronexp-1e721.firebaseapp.com",
+  databaseURL:       "https://dronexp-1e721-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId:         "dronexp-1e721",
+  storageBucket:     "dronexp-1e721.firebasestorage.app",
+  messagingSenderId: "181742674202",
+  appId:             "1:181742674202:web:4b7674401ba0fd502dcca3"
+};
